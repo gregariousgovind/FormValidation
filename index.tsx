22a16ff748a0641c2b354f871e933b5cd3e4059e
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-10 00:57:38 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-import SimpleInput from './src/components/SimpleInput';
+// import SimpleInput from './src/components/SimpleInput';
+import BasicForm from './src/components/BasicForm';
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
